8daf2afe43fb08ea6946c278a4134e0fa2efcf1f
11joelrose <rosa0379@student.acitech.org>
2025-03-21 09:13:17 -0400

    I started making my JavaScript Still need to finish the JavaScript Nothing seems to work and I dont really understand the material

---
+++ scirpt.js
@@ -0,0 +1,48 @@
+const rock = document.getElementById('1')
+const paper = document.getElementById('2')
+const scissors = document.getElementById('3')
+const stapler = document.getElementById('4')
+const tape = document.getElementById('5')
+
+const button = document.querySelector('button');
+button.addEventListener('click', myFunction);
+
+if (player chose 1)
+    if (cpu chose 3 or cpu chose 4)
+        "Player Wins!"
+    else if (cpu chose 1)
+        "Tie!"
+    else
+        "player Loses!"
+
+if (player chose 2)
+    if (cpu chose 1 or cpu chose 5)
+        "Player Wins!"
+    else if (cpu chose 2)
+        "Tie!"
+    else
+        "Player Loses!"
+
+if (player chose 3)
+    if (cpu chose 2 or cpu chose 4)
+        "Player Wins!"
+    else if (cpu chose 3)
+        "Tie!"
+    else
+        "Player Loses!"
+
+if (player chose 4) 
+    if (cpu chose 2 or cpu chose 5)
+        "Player Wins!"
+    else if (cpu chose 4)
+        "Tie!"
+    else
+        "Player Loses!"
+
+if (player chose 5)
+    if (cpu chose 3 or cpu chose 1)
+        "Player Wins!"
+    else if (cpu chose 5)
+        "Tie!"
+    else
+        "Player Loses!"
