f610dd07477458be9c1744765ecf822c3d55504b
11joelrose <rosa0379@student.acitech.org>
2025-04-04 08:55:06 -0400

    finished the project fully and everything works well think I did a good job and this is my best work so far

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Rock paper scissors
-
+//variables//
 const choices = ["rock", "paper", "scissors", "tape", "stapler"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
@@ -11,7 +11,7 @@ let playerScore = 0;
 let computerScore = 0; 
 
 function playGame(playerChoice){
-
+    //allows the computer' decision to work propely//
     const computerChoice = choices[Math.floor(Math.random() * 3)];
     let result ="";
 
@@ -38,36 +38,46 @@ function playGame(playerChoice){
                     break;
         }
     }
-
+    //allows the statements for the game to function work//
     playerDisplay.textContent = `PLAYER: ${playerChoice}`;
     computerDisplay.textContent = `computer: ${computerChoice}`;
     resultDisplay.textContent = result;
-
+    //allows the color change depending on the outcome to work, (for win or lose scenarios not TIEs) 
     resultDisplay.classList.remove("greenText", "redText")
 
     switch(result){
+        //this changes the color depedning on the outcome and decision made by the player in this case if the player wins//
         case "YOU WIN!":
+            resultDisplay.classList.remove ("darkText");
             resultDisplay.classList.add("greenText");
             playerScore++;
             playerScoreDisplay.textContent = playerScore;
+            resultDisplay.classList.remove ("darkText");
             resultDisplay.classList.add("greenText");
             resultDisplay.classList.remove("redText");
             break;
+            // this changes the color depending on the outcome and decision made by the player in this case if the player loses//
         case "YOU LOSE!":
+            resultDisplay.classList.remove ("darkText");
             resultDisplay.classList.add("redText");
             computerScore++;
             computerScoreDisplay.textContent = computerScore;
+            resultDisplay.classList.remove ("darkText");
             resultDisplay.classList.add("redText");
             break;
     }
+    //this is the reset function and allows the game's score to be reset and refreshes the socre, it also promps up a message to the player that says select  item, this guides the player and adds a customization that makes it cool!//
     function Reset(){
         playerScore = 0;
         computerScore = 0;
         playerScoreDisplay.innerHTML = 0;
         computerScoreDisplay.innerHTML=0;
         resultDisplay.innerHTML= "Select a item!";
+        resultDisplay.classList.add ("darkText");
+        resultDisplay.classList.remove("greenText");
+        resultDisplay.classList.remove("redText");
         console.log (playerScore)
-
     }
+    //even listener that allows the button for the reset function to work//
     resetButton.addEventListener("click", Reset)
 }
